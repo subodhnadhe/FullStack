2e2bb9c652bc4559348a4ded6f94afc858922d48
subodhnadhe <nadhe.subodh76@gmail.com>
2026-08-17 02:59:57 +0530

    Add JavaScript learning code

---
+++ project1/project1.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+
+buttons.forEach((button)=>{
+    console.log(button);
+    button.addEventListener('click',(e)=>{
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === "grey"){
+            body.style.backgroundColor = e.target.id;
+        }
+         if(e.target.id === "purple"){
+            body.style.backgroundColor = e.target.id;
+        }
+         if(e.target.id === "blue"){
+            body.style.backgroundColor = e.target.id;
+        }
+         if(e.target.id === "yellow"){
+            body.style.backgroundColor = e.target.id;
+        }
+         if(e.target.id === "default"){
+            body.style.backgroundColor = 'white';
+        }
+    });
+});
